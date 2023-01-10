76e33dc16728292b4952e65e075679d808947974
vominhthongss <vominhthongss@gmail.com>
2023-01-10 09:11:43 +0700

    Change The renderSquare Method In Board

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,22 +2,10 @@ import React, { Component } from "react";
 import "./Square.css";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          this.setState({ value: "X" });
-        }}
-      >
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
